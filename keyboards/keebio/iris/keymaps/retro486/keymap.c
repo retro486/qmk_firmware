38f606826ba1845c1b1f0ee916f1df43e325e51e
Russ Bernhardt <russell.bernhardt@gmail.com>
2019-12-09 22:15:43 -0800

    Changes in modifiers after first full day at work trying to use this layout. Move popular keys to dedicated slots and move macros to alt layout since they're rarely used.

--- keyboards/keebio/iris/keymaps/retro486/keymap.c
+++ keyboards/keebio/iris/keymaps/retro486/keymap.c
@@ -12,27 +12,19 @@ enum layer_number {
 
 // One-shots
 #define OS_LSFT OSM(MOD_LSFT)
+#define OS_RSFT OSM(MOD_RSFT)
 
 // Tap dance
 enum tapdances{
-  TD_SPENT = 0,
-  TD_BDEL,
-  TD_ZP,
-  TD_ND,
-  TD_LBRB,
+  TD_ZP = 0,
+  TD_LBRB
 };
 
-#define KC_SPENT  TD(TD_SPENT)
-#define KC_BDEL  TD(TD_BDEL)
 #define KC_ZP TD(TD_ZP)
-#define KC_ND TD(TD_ND)
 #define KC_LBRB TD(TD_LBRB)
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_SPENT] = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_ENT),
-  [TD_BDEL] = ACTION_TAP_DANCE_DOUBLE(KC_BSPC, KC_DEL),
   [TD_ZP] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_EQL),
-  [TD_ND] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_MINS),
   [TD_LBRB] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC)
 };
 
@@ -40,15 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_ND,    KC_ZP,    KC_BDEL,
+     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_ZP,   KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     OS_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    DM_PLY1,          TG_COLEMAK, KC_N,  KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_LBRB,
+     KC_RALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS,          TG_COLEMAK, KC_N,  KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_LBRB,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    DM_REC1, DM_RSTP,  TT_ALT,                    TT_ALT, KC_SPENT, KC_RALT
+                                    OS_LSFT, TT_ALT,  KC_ENT,                     TT_ALT,  KC_SPC, OS_RSFT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -68,15 +60,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ALT] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD,  KC_F1,    KC_F2,  KC_F3,   KC_F4,  _______,                            _______, _______,  KC_F10,  KC_F11,  KC_F12, _______,
+     KC_TILD,  KC_F1,    KC_F2,  KC_F3,   KC_F4,  _______,                            _______, _______,  KC_F10,  KC_F11,  KC_F12,   KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_GRV,  _______, KC_HOME,  KC_UP,   KC_END, _______,                             KC_P7,   KC_P8,   KC_P9,  _______, _______, _______,
+     KC_GRV,  _______, KC_PGDN,  KC_UP,   KC_PGUP, _______,                             KC_P7,   KC_P8,   KC_P9,  _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_PGUP, KC_LEFT, KC_DOWN ,KC_RGHT , KC_PGDN,                             KC_P4,  KC_P5,   KC_P6,  _______, _______, _______,
+     _______, KC_HOME, KC_LEFT, KC_DOWN ,KC_RGHT , KC_END,                             KC_P4,  KC_P5,   KC_P6,  _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______, _______,           KC_P0,   KC_P1,   KC_P2,   KC_P3,  _______, _______, _______,
+     _______, _______, _______, _______, _______, _______,  DM_PLY1,          KC_P0,   KC_P1,   KC_P2,   KC_P3,  _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______,  _______, _______
+                                     DM_REC1,_______, DM_RSTP,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
